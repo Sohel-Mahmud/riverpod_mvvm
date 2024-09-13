b21345df90824acd85ca2632444f350d3707827c
sohel-mahmud <sohel7avon@gmail.com>
2024-09-13 17:51:19 +0600

    directory moved up

---
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:soptify_mvvm_riverpod/core/theme/app_pallete.dart';
+import 'package:soptify_mvvm_riverpod/core/widgets/custom_fields.dart';
+import 'package:soptify_mvvm_riverpod/features/auth/view/widgets/auth_gradient_button.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign Up',
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 15),
+              CustomField(hintText: "Name", controller: nameController),
+              const SizedBox(height: 15),
+              CustomField(hintText: "Email", controller: emailController),
+              const SizedBox(height: 15),
+              CustomField(hintText: "Password", controller: passwordController, isObscureText: true,),
+              const SizedBox(height: 20),
+              AuthGradientButton(buttonText: "Sign Up", onTap: () {}),
+              const SizedBox(height: 20),
+              RichText(
+                  text: TextSpan(
+                text: "Already have an account? ",
+                style: Theme.of(context).textTheme.titleMedium,
+                children: const [
+                  TextSpan(
+                    text: "Sign In",
+                    style: TextStyle(
+                      color: Pallete.gradient2,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- client/soptify_mvvm_riverpod/android/app/src/main/kotlin/com/example/soptify_mvvm_riverpod/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.soptify_mvvm_riverpod
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
